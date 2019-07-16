b961f0c6a62aba81f6b4a7b54ad59240ea273a98
Dan Smith <daniel.smith8795@gmail.com>
2019-07-16 11:06:52 -0500

    final working product, adjusted for new date structure

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -25,22 +25,19 @@ var monthlyRate = 0;
 var totalBilled = 0;
 
 $("#submit-form").on("click", function (event) {
-    //var input=0;
-    //input++;
+
     event.preventDefault();
-    alert("button works");
     name = $("#employee-name").val().trim();
     role = $("#role").val().trim();
     startDate = $("#start-date").val().trim();
     monthlyRate = parseFloat($("#monthly-rate").val().trim());
     //totalBilled = $("#total-billed").val().trim();
-    $(".table").empty();
+    //$(".table").empty();
     database.ref().push({
         name: name,
         role: role,
         startDate: startDate,
-        monthlyRate: monthlyRate,
-        //totalBilled: totalBilled
+        monthlyRate: monthlyRate
     });
 
 })
@@ -51,24 +48,9 @@ database.ref().on("child_added", function (snapshot) {
     console.log(sv.role);
     console.log(sv.startDate);
     console.log(sv.monthlyRate);
-    //console.log(sv.totalBilled);
 
-    //$("#employee-name").text(sv.name);
-    //$("#role").text(sv.role);
-    //$("#start-date").text(sv.startDate);
-    //$("#monthlyRate").text(sv.monthlyRate);
-    //$("#totalBilled").text(sv.totalBilled);
-
-    //$(".table").empty();
-    var ref = database.ref();
     console.log(snapshot.key + " - name is " + snapshot.val().name);
     createRow(snapshot.val());
-    /*ref.orderByChild("height").on("child_added", function (snapshot) {
-        console.log(snapshot.key + " - name is " + snapshot.val().name);
-
-        createRow(snapshot.val());
-    });*/
-
 
 
 }, function (errorObject) {
@@ -78,40 +60,25 @@ database.ref().on("child_added", function (snapshot) {
 
 
 function createRow(data) {
-    //var body = $('body')[0];
+    var startDataFormat = "YYYY/MM/DD";
+    var convertedDate = moment(data.startDate, startDataFormat);
+    console.log("start date: " + convertedDate);
+
+    //var readablestartDate=data.startDate
+    var monthsWorked = -convertedDate.diff(moment(), "months");
+    console.log(monthsWorked);
+    var monthlyRate = data.monthlyRate;
+    var totalBilled = monthsWorked * monthlyRate;
     var tbl = $('.table');
-    //var tblBody = $('tbody');
 
-    //for (var j = 0; j <= 2; j++) {
-    //var newTR = $('<tr>');
-
-    //for (var i = 0; i < 2; i++) {
-    //var cell = $('<td>');
-    //var cellText = $('cell is row ' + j + ', column ' + i);
-
-    //cell.text();
-    //cell.append(cellText);
     var row = $('<tr>');
     row.append($('<td>').text(data.name));
     row.append($('<td>').text(data.role));
     row.append($('<td>').text(data.startDate));
-    row.append($('<td>')); //monthsWorked
+    row.append($('<td>').text(monthsWorked));
     row.append($('<td>').text(data.monthlyRate));
-    row.append($('<td>')); //totalBilled
-    //}
+    row.append($('<td>').text(totalBilled));
 
     tbl.append(row);
 
-    //}
-
-    //tbl.append(tblBody);
-    //body.append(tbl);
-    //tbl.attr('border', '2');
 }
-
-
-
-
-//firebase info path "role" = i.node_.children_.root_.value.value_
-//"name" = i.node_.children_.root_.left.value.value_
-//
